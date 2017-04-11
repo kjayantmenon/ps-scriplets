0c0b8e26f38d5e25ba2c531257bea4809146425b
H190242 <jayant.kizhepatt@honeywell.com>
2017-04-11 13:16:06 +0530

    added getallresources script

---
+++ README.md
@@ -0,0 +1,2 @@
+These are a set of powershell scripts to enable simple tasks like 
+- A formatted list of resources and resource types in a resource group
---
+++ getallrmresources.ps1
@@ -0,0 +1,23 @@
+﻿
+Select-AzureRmProfile -Path [path to the azure profile json file]
+
+Select-AzureRmSubscription -SubscriptionId [Azure subscription id]
+Get-AzureRmResourceGroup
+
+$rgName="[resource group name]"
+$Tab = [char]9
+$resources = Get-AzureRmResource
+
+$line="Resource Name".PadRight(40,' ') +$Tab+$Tab+ "|" +$Tab+$Tab+ "Resource Type"
+Write-Host $line
+$line="_".PadRight(100,'_')
+Write-Host $line
+foreach($resource in $resources)
+{
+    if($resource.ResourceGroupName –eq $rgName){
+        $resoureName=$resource.Name.PadRight(40,' ')
+        
+        $line=$resoureName +$Tab+$Tab+ "|" +$Tab+$Tab+ $resource.ResourceType
+        Write-Host $line
+    }
+}
