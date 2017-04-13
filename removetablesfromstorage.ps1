fa458b10e57247d6d05298fad1517ed66cd73a95
H190242 <jayant.kizhepatt@honeywell.com>
2017-04-13 11:21:11 +0530

    added the remove all tables command

---
+++ removetablesfromstorage.ps1
@@ -0,0 +1,18 @@
+﻿#About::To remove tables from a storage account
+
+Write-Host "Delete Start!"
+$StorageAccountName = "[storage account name]"
+$StorageAccountKey = "[account key]"
+
+#$Ctx = New-AzureStorageContext –StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey
+
+$ctx = New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey
+$tables =Get-AzureStorageTable -Context $ctx
+
+foreach($table in $tables)
+{
+    $tabName = $table.Name
+    Remove-AzureStorageTable –Name $tabName –Context $Ctx -Force
+    Write-Host "Deleted table "+$tabName
+    #$tclient.DeleteTable($table)
+}
